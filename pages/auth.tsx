9384efea6a3294dfee1848f55006578de23dde8a
manuhutu344 <nandoperdos2@gmail.com>
2023-06-29 11:51:56 +0900

    Berhasil membuat login menggunakan github

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -68,7 +68,7 @@ const Auth = () =>{
                             </div>
                         </div>
                         <div className="flex flex-row items-center gap-4 mt-8 justify-center">
-                            <div className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+                            <div onClick={() =>signIn('github', {callbackUrl: '/'})} className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
                                 <FaGithub size={30} />
                             </div>
                         </div>
