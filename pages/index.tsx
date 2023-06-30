1e808a32def3f5bb67277d9ae434275767fa88a0
manuhutu344 <nandoperdos2@gmail.com>
2023-06-30 11:07:55 +0900

    Membuat logout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
-import Head from 'next/head'
+import {signOut} from "next-auth/react"
+
 export default function Home() {
   return (
     <>
-      <h1 className='text-2xl text-green-500'>Nonton Online</h1>
+      <h1 className='text-4xl text-green-500'>Nonton Online</h1>
+      <button className="h-10 w-full bg-white" onClick={()=>signOut()}>Logout</button>
     </>
   )
 }
