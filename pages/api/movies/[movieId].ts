1acc3e895d50507b76231754162806d9e7cf2826
manuhutu344 <nandoperdos2@gmail.com>
2023-07-17 11:37:02 +0900

    Membuat fungsi play

---
+++ pages/api/movies/[movieId].ts
@@ -0,0 +1,31 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import prismadb from '@/lib/prismadb'
+import serverAuth from "@/lib/serverAuth";
+
+export default async function handler(req:NextApiRequest, res:NextApiResponse){
+    if(req.method !== 'GET'){
+        return res.status(405).end()
+    }
+    try {
+        await serverAuth(req, res)
+        const {movieId} = req.query
+        if(typeof movieId !== 'string'){
+            throw new Error('ID Tidak Valid')
+        }
+        if(!movieId){
+            throw new Error('Id Tidak Valid')
+        }
+        const movie = await prismadb.movie.findUnique({
+            where:{
+                id: movieId
+            }
+        })
+        if(!movie){
+            throw new Error('Id Tidak Valid')
+        }
+        return res.status(200).json(movie)
+    } catch (error) {
+        console.log(error)
+        return res.status(400).end()
+    }
+}
