c6a7e54ac08b6efb95da4b0c4bf5aac6febdf0df
manuhutu344 <nandoperdos2@gmail.com>
2023-07-18 12:10:56 +0900

    Merubah cssnya

--- components/InfoModal.tsx
+++ components/InfoModal.tsx
@@ -27,7 +27,7 @@ const InfoModal: React.FC<InfoModalProps> = ({visible, onClose}) =>{
         return null
     }
     return(
-        <div className='z-50 transition duration-300 bg-black bg-opacity-80 flex justify-center items-center overflow-x-hidden overflow-y-auto fixed insert-0'>
+        <div className='z-50 transition duration-300 bg-black bg-opacity-80 flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0'>
             <div className='relative w-auto mx-auto max-w-3xl rounded-md overflow-hidden'>
                 <div className={`${isVisible ? 'scale-100' : 'scale-0'} transform duration-300 relative flex-auto bg-zinc-900 drop-shadow-md`}>
                     <div className='relative h-96'>
